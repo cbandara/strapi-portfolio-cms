ba45edefa1cd21dd6c5adcc313bba4a75de605b1
Charutha <charuthabandara@gmail.com>
2020-10-02 23:15:21 -0400

    inital commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c46a475dbfa12aafa5aa0c101ded9bd5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd073b523-5f92-4979-8823-beb5e31abe74'
+};
